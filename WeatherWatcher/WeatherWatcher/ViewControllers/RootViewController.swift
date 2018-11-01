7c5ba99747925e239bbd3817b44019d78ab5d0ca
Rick Williams <risingsun66@gmail.com>
2018-11-01 16:27:40 -0700

    Project setup

---
+++ WeatherWatcher/WeatherWatcher/ViewControllers/RootViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RootViewController.swift
+//  WeatherWatcher
+//
+//  Created by Rick Williams on 11/1/18.
+//  Copyright © 2018 Rick Williams. All rights reserved.
+//
+
+import UIKit
+
+final class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
